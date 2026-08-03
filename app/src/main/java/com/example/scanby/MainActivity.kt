4e01304be837170945cffe25a3a244d81a923794
SJG <shinjg97@gmail.com>
2026-08-03 18:27:41 +0900

    [feat] onboarding screen 으로 시작 - 추후 영구저장데이터 활용하여 매번 시작하지 않도록 수정해야됨

--- app/src/main/java/com/example/scanby/MainActivity.kt
+++ app/src/main/java/com/example/scanby/MainActivity.kt
@@ -4,44 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.scanby.core.designsystem.theme.ScanbyTheme
+import com.example.scanby.feature.onboarding.OnboardingScreen
+import com.example.scanby.navigation.OnboardingRoute
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ScanbyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = OnboardingRoute.route) {
+                    composable(OnboardingRoute.route) {
+                        OnboardingScreen(
+                            onFinished = {
+                                // TODO: navigate to feature.login / feature.home once they exist
+                            },
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ScanbyTheme {
-        Greeting("Android")
     }
 }
